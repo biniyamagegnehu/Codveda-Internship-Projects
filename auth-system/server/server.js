d60e143446cdf35ba72b51849ad818750a67e12d
Biniyam Agegnehu <biniyamagegnehu2@gmail.com>
2026-03-12 22:00:58 +0300

    Update server.js to include protected routes

--- auth-system/server/server.js
+++ auth-system/server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const protectedRoutes = require('./routes/protectedRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -22,6 +23,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/protected', protectedRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
